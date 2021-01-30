653c5a2ee94c209c7eac5b1144a26aff23dd466d
daniele.pelosi <danielepelosi99@gmail.com>
2021-01-30 14:30:05 +0100

    non funziona la registrazione

--- src/main/java/it/unicam/ids/c3/gestori/GestoreClienti.java
+++ src/main/java/it/unicam/ids/c3/gestori/GestoreClienti.java
@@ -1,6 +1,8 @@
 package it.unicam.ids.c3.gestori;
 
 import it.unicam.ids.c3.merce.Categoria;
+import it.unicam.ids.c3.merce.Merce;
+import it.unicam.ids.c3.merce.MerceAlPubblico;
 import it.unicam.ids.c3.merce.MerceInventarioNegozio;
 import it.unicam.ids.c3.negozio.Negozio;
 import it.unicam.ids.c3.persistenza.ClienteRepository;
@@ -47,10 +49,20 @@ public class GestoreClienti {
     }
 
     /*****************Consulta Promozioni******************/
+
     public List<MerceInventarioNegozio> getPromozioni() {
         List<MerceInventarioNegozio> min = new ArrayList<>();
-        for(Negozio negozio : negozioRepository.findAll()){
-            min.addAll(gestoreMerci.getPromozioniAttive(negozio));
+        Iterator<Negozio> negozioIterator = negozioRepository.findAll().iterator();
+        while (negozioIterator.hasNext()){
+            Negozio negozio = negozioIterator.next();
+            Iterator<MerceInventarioNegozio> meinc = negozio.getMerceInventarioNegozio().iterator();
+            while (meinc.hasNext()){
+//                MerceInventarioNegozio merceInventarioNegozio = meinc.next();
+//                if(merceInventarioNegozio.getMerceAlPubblico().getPromozione().isDisponibile()){
+//                    min.add(merceInventarioNegozio);
+//                }
+                System.out.println(" PER FAVOREEEEEEEEEEEEEEEEEEEEE");
+            }
         }
         return min;
     }
